ccaec4b392642615a8dc1b3782c9c6149b22e267
erenctn <eren__cetin__@hotmail.com>
2022-04-17 01:28:39 +0300

    First commit

--- catalog/urls.py
+++ catalog/urls.py
@@ -19,12 +19,19 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('',include('pages.urls')),
     path('movies/',include('movies.urls')),
     path('user/',include('user.urls')),
     path('admin/', admin.site.urls),
     path('video/', include('video.urls')),
+
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 if settings.DEBUG:
